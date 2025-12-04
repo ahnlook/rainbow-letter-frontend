cf21f63ae30eff549f0ffc318b45801f52b77369
ahnlook <ahnlook90@gmail.com>
2025-12-04 05:06:29 +0000

    alt 수정

--- src/components/Donate/Welcome.tsx
+++ src/components/Donate/Welcome.tsx
@@ -4,7 +4,7 @@ import Logo from 'assets/im_landing_logo.png';
 export default function Welcome() {
   return (
     <header className="flex flex-col items-center justify-center pt-8">
-      <img src={Logo} alt="로고 이미지" />
+      <img src={Logo} alt="logo" />
       <h1 className="text-heading-2 font-bold">무지개편지를 도와주세요!</h1>
     </header>
   );
