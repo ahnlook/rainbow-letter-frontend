9e68f54b876c5919f46e3c0cdf1afc403aa70c97
ahnlook <ahnlook90@gmail.com>
2026-02-22 08:31:06 +0000

    Revert "feat: add AIT button test page and route"
    This reverts commit 18d6e5fc2549ca032d5e4c96669dea3b834e6268.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { TDSMobileAITProvider } from '@toss/tds-mobile-ait';
 
 import App from 'App';
 import store from 'store/index';
@@ -8,32 +7,6 @@ import './index.css';
 import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-
-const isAitRuntime = () => {
-  if (process.env.REACT_APP_ENABLE_AIT_PROVIDER !== 'true') {
-    return false;
-  }
-
-  if (typeof window === 'undefined') {
-    return false;
-  }
-
-  return Boolean(window.ReactNativeWebView);
-};
-
-const renderApp = () => {
-  const app = (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  if (isAitRuntime()) {
-    return <TDSMobileAITProvider>{app}</TDSMobileAITProvider>;
-  }
-
-  return app;
-};
 
 if ('service-worker' in navigator) {
   window.addEventListener('load', () => {
@@ -51,4 +24,8 @@ if ('service-worker' in navigator) {
   });
 }
 
-root.render(renderApp());
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
