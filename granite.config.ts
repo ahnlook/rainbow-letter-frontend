063a67e098f538d3b75ae2cea65dc0ff2e615443
ahnlook <ahnlook90@gmail.com>
2026-02-22 08:25:15 +0000

    feat: add AIT button test page and route

--- granite.config.ts
+++ granite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   brand: {
     displayName: '무지개편지', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: '', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: 'https://static.toss.im/appsintoss/8603/6766c896-da95-40b6-836e-749e26b1171d.png', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
     host: 'localhost',
